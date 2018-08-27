d51cec2fc93fae51bc904b54b569e8ae64edb91b
Andre Noberto <andreno.souza@gmail.com>
2018-08-27 14:41:06 -0300

    Refactoring code, creating files according to payload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -9,15 +9,11 @@ const cssRouter = require('./routes/css');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/css', cssRouter);
--- routes/css.js
+++ routes/css.js
@@ -2,12 +2,32 @@ const express = require('express');
 const router = express.Router();
 const runGulpTask = require('run-gulp-task');
 const path = require('path');
+const fs = require('fs');
 
-router.get('/', (req, res) => {
+const writeFile = (fileName, data) => {
+    const filePath = path.join(process.cwd(), `src/styles/variables/_${fileName}.scss`);
+    fs.writeFileSync(filePath, '');
+    if (data.imports) {
+        for (let i in data.imports) {
+            fs.appendFileSync(filePath, `@import "${data.imports[i]}";\n`);
+        }
+    }
+    for (let i in data) {
+        if (i === 'imports') continue;
+        fs.appendFileSync(filePath, `$${i}: ${data[i]};\n`);
+    }
+}
+
+router.post('/', (req, res) => {
+    if (req.body) {
+        for (let i in req.body) {
+            writeFile(i, req.body[i]);
+        }
+    }
     runGulpTask('default', path.join(process.cwd(), 'gulpfile.js'))
         .then(() => {
             console.log('task completed');
-            res.status(200).json({compiled: true});
+            res.status(200).json({ compiled: true });
         })
         .catch(err => {
             console.log('Error', err);
